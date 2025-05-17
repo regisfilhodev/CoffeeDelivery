ffe9cf7a83f786aa63f6f85412b8205da92a6de8
Regis Filho <filhoregis96@gmail.com>
2025-05-16 22:00:25 -0300

    Atividade

---
+++ src/app.service.ts
@@ -0,0 +1,59 @@
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './types/Coffees';
+
+const coffeeList = [
+  {
+    id: 1,
+    nome: 'Café com leite',
+    tipo: '1',
+    descricao: 'O melhor',
+    preco: 6,
+    tag: ['Normal', 'Quente'],
+    quantidade: 3
+  },
+  {
+    id: 2,
+    nome: 'Capuccino',
+    tipo: '2',
+    descricao: 'Café cremoso com toque de chocolate',
+    preco: 8,
+    tag: ['Doce', 'Cremoso', 'Quente'],
+    quantidade: 5
+  },
+  {
+    id: 3,
+    nome: 'Café Gelado',
+    tipo: '3',
+    descricao: 'Refrescante para dias quentes',
+    preco: 7,
+    tag: ['Gelado', 'Refrescante'],
+    quantidade: 2
+  }
+]  
+
+@Injectable()
+export class AppService {
+
+  getHello(): string {
+    return 'Lista de cafés';
+  }
+  getCoffee(): Coffee[] {
+    return coffeeList;
+  }
+  getCoffeeId(id: number): Coffee[] {
+    const coffee = coffeeList.filter(coffee => coffee.id === Number(id))
+    if (coffee.length === 0) {
+      throw new NotFoundException(`Café com ID ${id} não encontrado!`);
+    }
+    return coffee;   
+  }
+  postCoffee(coffee: Coffee): Coffee[] {
+    const coffeeExists = coffeeList.some(c => c.id === coffee.id);
+    if (coffeeExists) {
+      throw new ConflictException(`Café com ID ${coffee.id} já existe!`);
+    }
+    coffeeList.push(coffee)
+
+    return coffeeList;
+  }
+}
---
+++ src/types/Coffees.ts
@@ -0,0 +1,9 @@
+export type Coffee = {
+    nome: string;
+    tipo: string;
+    id: number;
+    descricao: string;
+    preco: number;
+    tag: string[] ;
+    quantidade: number;
+}
